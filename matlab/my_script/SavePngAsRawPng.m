62dd5eb8e46a7a2f27d9f73575816de6eda0c04c
George Papandreou <george.papandreou@gmail.com>
2015-01-25 22:36:24 -0800

    Jay's matlab files

--- matlab/my_script/SavePngAsRawPng.m
+++ matlab/my_script/SavePngAsRawPng.m
@@ -7,8 +7,8 @@ clear all; close all;
 %orig_folder = '/rmt/work/deeplabel/exper/voc12/res/erode_gt/post_densecrf_W41_XStd33_RStd4_PosW3_PosXStd3/results/VOC2012/Segmentation/comp6_trainval_aug_cls';
 %save_folder = '/rmt/data/pascal/VOCdevkit/VOC2012/SegmentationClassBboxErodeCRFAug';
 
-orig_folder = '/rmt/data/pascal/VOCdevkit/VOC2012/SegmentationClassBboxErode20CRFAug_Visualization';
-save_folder = '/rmt/data/pascal/VOCdevkit/VOC2012/SegmentationClassBboxErode20CRFAug';
+orig_folder = '/rmt/data/pascal/VOCdevkit/VOC2012/SegmentationClassBboxErode20_OccluBiasCRFAug_Visualization';
+save_folder = '/rmt/data/pascal/VOCdevkit/VOC2012/SegmentationClassBboxErode20_OccluBiasCRFAug';
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
--- matlab/my_script/SetupEnv.m
+++ matlab/my_script/SetupEnv.m
@@ -5,8 +5,8 @@ clear all; close all;
 load('./pascal_seg_colormap.mat');
 
 is_server       = 1;
-is_mat          = 1;   % the results are saved as mat or png
-has_postprocess = 0;   % has done densecrf post processing or not
+is_mat          = 0;   % the results are saved as mat or png
+has_postprocess = 1;   % has done densecrf post processing or not
 is_argmax       = 0;   % the output has been taken argmax already (e.g., coco dataset). 
                        % assume the argmax takes C-convention (i.e., start from 0)
 
@@ -36,9 +36,18 @@ debug           = 0;   % if debug, show some results
 % vgg128_noup_pool3_bbox_crf
 % bi_w = 7, bi_x_std = 41, bi_r_std = 7
 
+% vgg128_noup_pool3_bbox_crf2
+% bi_w = 5, bi_x_std = 77, bi_r_std = 3, pos_w = 3, pos_x_std = 3
+
 % vgg128_noup_pool3_adaweak (1st no cross-validate)
 % bi_w = 5, bi_x_std = 50, bi_r_std = 10
 % bi_w = 35, bi_x_std = 61, bi_r_std = 10
+
+% vgg128_noup_pool3_strongweak2
+% bi_w = 5, bi_x_std = 81, bi_r_std = 5, pos_w = 3, pos_x_std = 3
+
+% vgg128_noup_pool3_small
+% bi_w = 5, bi_x_std = 61, bi_r_std = 3, pos_w = 3, pos_x_std = 3
 
 % erode_gt (bbox)
 % bi_w = 41, bi_x_std = 33, bi_r_std = 4
@@ -50,23 +59,26 @@ debug           = 0;   % if debug, show some results
 % bi_w = 45, bi_x_std = 37, bi_r_std = 3, pos_w = 15, pos_x_std = 3
  
 
-pos_w          = 15;    %3
+pos_w          = 3;    %3
 pos_x_std      = 3;    %3
 
-bi_w           = 45;   %5;   3
-bi_x_std       = 37;  %67;   %50;  95
+bi_w           = 5;   %5;   3
+bi_x_std       = 61;  %67;   %50;  95
 bi_r_std       = 3;   %3;    %10;  3
 
 
 dataset    = 'voc12';  %'voc12', 'coco'
 id         = 'comp6';
-trainset   = 'train_aug';
+trainset   = 'trainval_aug';
 testset    = 'val';            %'val', 'test'
 
-model_name = 'vgg128_noup_pool3_adaweakcrf2';   %'vgg128_noup', 'vgg128_noup_glob', 'vgg128_ms'
+model_name = 'vgg128_noup_pool3_small';   %'vgg128_noup', 'vgg128_noup_glob', 'vgg128_ms'
 
-feature_name = 'erode_gt';    %'';    %'features', 'features4', 'features2', ''
-feature_type = 'bboxErode20_OccluBias';     %'bboxErode20';   %'fc8', 'crf', 'fc8_crf', 'erode_gt', 'erode_gt_mask'
+feature_name = 'features';
+feature_type = 'fc8';
+
+% feature_name = 'erode_gt';     % 'erode_gt', 'features', 'features4', 'features2', ''
+% feature_type = 'bboxErode20_OccluBias';        %'bboxErode20', 'fc8', 'crf', 'fc8_crf'
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% used for cross-validation
@@ -79,11 +91,11 @@ down_sample_rate   = 8;
 num_sample         = 100;     % used for erode_gt 
 
 % ranges for cross-validation
-range_pos_w = 3;
-range_pos_x_std = 3;
+range_pos_w = 1;
+range_pos_x_std = 1;
 
-range_bi_w = 30:10:50;
-range_bi_x_std = 30:10:50;
-range_bi_r_std = [3 5];
+range_bi_w = [5];
+range_bi_x_std = 81;
+range_bi_r_std = [5];
 
 
