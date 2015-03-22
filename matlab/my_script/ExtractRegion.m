a0fe8fec6f8d12f7d3a2a19091c699e791c56370
Liang-Chieh Chen <jay.lcchen@gmail.com>
2015-03-22 23:02:40 +0000

    ExtractRegion.m deleted online with Bitbucket

--- matlab/my_script/ExtractRegion.m
+++
@@ -1,3 +0,0 @@
-function out = ExtractRegion(in, kernel, stride)
-out = in( ceil(kernel/2) : stride : size(in,1) - floor(kernel/2), ...
-                 ceil(kernel/2) : stride : size(in,2) - floor(kernel/2) );
