0e9a1349c25a7a6d075fc7165ade1cada16a15d0
Ali <ali@alanlab-linux-1.(none)>
2015-08-26 22:19:45 -0700

    fixed error with running train

--- python/my_script/tools.py
+++ python/my_script/tools.py
@@ -60,7 +60,8 @@ def file_editor(filein, train_set='', test_set=''):
 	newdata = newdata.replace('${NET_ID}', os.environ['NET_ID'])
 	newdata = newdata.replace('${TRAIN_SET}', train_set)
 	newdata = newdata.replace('${TEST_SET}', test_set)
-	newdata = newdata.replace('${FEATURE_DIR}', os.environ['FEATURE_DIR'])
+	if test_set != '':
+		newdata = newdata.replace('${FEATURE_DIR}', os.environ['FEATURE_DIR'])
 
 	f = open(filein,'w')
 	f.write(newdata)
