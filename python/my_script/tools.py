88424c11e190e048fae2f2018097e2bc1354aee3
TheLegendAli <amousavi14@gmail.com>
2016-02-18 14:09:40 -0500

    edited the deleted lines on tools.py

--- python/my_script/tools.py
+++ python/my_script/tools.py
@@ -3,25 +3,28 @@ import subprocess
 import wget
 import shutil
 import glob
+from distutils.dir_util import copy_tree
 
 def model_finder(path, type_=1):
-	mtime = lambda f: os.stat(os.path.join(path, f)).st_mtime
-	files = list(sorted(glob.glob(path+'/*.caffemodel'), key=mtime))
+	mtime = lambda f: os.stat(f).st_mtime
+	files = list(sorted(glob.glob(path+'/' + os.environ['MODEL'] + '_*.caffemodel'), key=mtime))
 	if len(files) >= 1:
 		file_ = files[-1]
 	else:
-		# TODO: need to change the url based on the type of experiment
-		url = 'http://ccvl.stat.ucla.edu/ccvl/init_models/vgg16_20M.caffemodel'
+		model_url = os.environ['MODEL'] + "-url"
+		url = os.environ[model_url]
 		print 'Downloading init caffemodel from ' + url
 		filename = wget.download(url)
-		file_ = path + '/init.caffemodel'
+		file_ = path + '/init_' + os.environ['MODEL'] + '.caffemodel'
 		shutil.move(filename, file_)
 
 	return file_
 
 
 def saver():#doesnt really save
-    model=os.environ['EXP'] + '/model/' + os.environ['NET_ID'] + '/test2.caffemodel'
+    # TODO: Check if the follwoing line works after dynamic prefixing the name.
+    # TODO: The model names should be  < os.environ['MODEL']_train*.caffemodel >
+    model=os.environ['EXP'] + '/model/' + os.environ['NET_ID'] + '/test2.caffemodel'  # TODO: Check if this works after dynamic prefixing the name
     if not os.path.isfile(model): model=model_finder(os.environ['EXP']+'/model/'+os.environ['NET_ID'])
     model_deploy=os.environ['EXP'] + '/model/' + os.environ['NET_ID'] + '/deploy.caffemodel'
 
@@ -34,6 +37,29 @@ def saver():#doesnt really save
 def environment_variable_creator(dic):
 	for key in dic.keys():
 		os.environ[key] = str(dic[key])
+
+def initializer():
+	mkdir()
+	get_urls()
+
+	current_path = os.getcwd()
+
+	try:
+		config_path = current_path + '/config/' + os.environ['MODEL']
+		copy_tree(config_path, os.environ['CONFIG_DIR'])
+	except:
+		print "\n you entered an invalid model \n"
+
+	model_path = current_path + '/init'
+	copy_tree(model_path, os.environ['MODEL_DIR'])
+
+	try:
+		list_path = current_path + '/list/' + os.environ['year'][-4:]
+		copy_tree(list_path, os.environ['LIST_DIR'])
+	except:
+		print "\n you entered an invalid year. \n"
+
+
 
 def mkdir():
 	CAFFE_DIR='./'
@@ -49,6 +75,8 @@ def mkdir():
 	    os.makedirs(MODEL_DIR), 
 	if not os.path.exists(LOG_DIR):
 	    os.makedirs(LOG_DIR)
+	if not os.path.exists(LIST_DIR):
+            os.makedirs(LIST_DIR)
 
 	os.environ['GLOG_log_dir'] = LOG_DIR
 	dic = {'CAFFE_DIR': CAFFE_DIR, 'CAFFE_BIN': CAFFE_BIN, 'CONFIG_DIR': CONFIG_DIR, 'MODEL_DIR': MODEL_DIR, 'LOG_DIR': LOG_DIR, 'LIST_DIR': LIST_DIR}
@@ -70,6 +98,7 @@ def file_editor(filein, train_set='', test_set=''):
 	newdata = newdata.replace('${EXP}', os.environ['EXP'])
 	newdata = newdata.replace('${TEST_SET}', test_set)
 	newdata = newdata.replace('${NUM_LABELS}', os.environ['NUM_LABELS'])
+	newdata = newdata.replace('${MODEL}', os.environ['MODEL'])
 	if test_set != '':
 		newdata = newdata.replace('${FEATURE_DIR}', os.environ['FEATURE_DIR'])
 
@@ -109,6 +138,7 @@ def matlab_path_editor(type_=1):
 
 	newdata = filedata.replace('{ROOT}', os.getcwd())
 	newdata = newdata.replace('{DATA_ROOT}', os.environ['DATA_ROOT'])
+	newdata = newdata.replace('{EXP}', os.environ['EXP'])
 	newdata = newdata.replace('{YEAR}', os.environ['year'])
 
 	
@@ -139,3 +169,14 @@ def matlab_result_runner():
 	subprocess.call("matlab -r 'EvalSegResults; exit;'", shell=True)
 	os.chdir(original_path)
 
+def get_urls():
+	urls = {}
+	urls['DeepLab-url'] = 'http://ccvl.stat.ucla.edu/ccvl/init_models/vgg16_128.caffemodel'
+	urls['DeepLab-COCO-LargeFOV-url'] = ''
+	urls['DeepLab-LargeFOV-url'] = 'http://ccvl.stat.ucla.edu/ccvl/init_models/vgg16_20M.caffemodel'
+	urls['DeepLab-MSc-url'] = ''
+	urls['DeepLab-MSc-COCO-LargeFOV-url'] = ''
+	urls['DeepLab-MSc-LargeFOV-url'] = 'https://dl.dropboxusercontent.com/u/2497281/DeepLab/DeepLab-LFOV.caffemodel'
+	urls['DeepLab-Weak-EM-Adapt-url'] = ''
+
+	environment_variable_creator(urls)
